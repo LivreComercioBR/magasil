3f493ab8dc1b05a833291a6ba68b591e4c18bbd7
Ronaldo Correia <ronaldo312006@hotmail.com>
2024-06-04 21:39:38 -0300

    alterado o carrinho para não criar o cliente

--- store/new_contexts.py
+++ store/new_contexts.py
@@ -7,24 +7,24 @@ from store.models import Produto, Pedido, Carrinho, Cliente, Categoria, Tipo
 
 def shopping_cart(request):
     qtd_prod_carrinho = 0
-    if request.user.is_authenticated:
-        cliente = request.user.cliente
-        # user.cliente da tabela OnetoOneField Client
-    else:
-        if request.COOKIES.get("id_sessao"):
-            id_sessao = request.COOKIES.get("id_sessao")
-            cliente, criado = Cliente.objects.get_or_create(
-                id_sessao=id_sessao)
-        else:
-            return {"qtd_prod_carrinho": qtd_prod_carrinho}
-    pedido, criado = Pedido.objects.get_or_create(
-        cliente=cliente, finalizado=False)
-    # Se tiver o pedido ele vai pegar o pedido, se não tiver, ele vai criar o pedido.
-    """Agora preciso saber quais são os itens do pedido"""
-    itens_carrinho = Carrinho.objects.filter(pedido=pedido)
-    """Posso ter um pedido com apenas 1 produto, porém, sendo 5 produtos do mesmo produto"""
-    for item in itens_carrinho:
-        qtd_prod_carrinho += item.quantidade
+    # if request.user.is_authenticated:
+    #     cliente = request.user.cliente
+    #     # user.cliente da tabela OnetoOneField Client
+    # else:
+    #     if request.COOKIES.get("id_sessao"):
+    #         id_sessao = request.COOKIES.get("id_sessao")
+    #         cliente, criado = Cliente.objects.get_or_create(
+    #             id_sessao=id_sessao)
+    #     else:
+    #         return {"qtd_prod_carrinho": qtd_prod_carrinho}
+    # pedido, criado = Pedido.objects.get_or_create(
+    #     cliente=cliente, finalizado=False)
+    # # Se tiver o pedido ele vai pegar o pedido, se não tiver, ele vai criar o pedido.
+    # """Agora preciso saber quais são os itens do pedido"""
+    # itens_carrinho = Carrinho.objects.filter(pedido=pedido)
+    # """Posso ter um pedido com apenas 1 produto, porém, sendo 5 produtos do mesmo produto"""
+    # for item in itens_carrinho:
+    #     qtd_prod_carrinho += item.quantidade
 
     return {"qtd_prod_carrinho": qtd_prod_carrinho}
 
